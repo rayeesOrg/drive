c32f8ccb57e375f2678369ebb0eaaf8b67c3025c
obaid <obaid@ip-172-31-13-77.us-west-2.compute.internal>
2015-08-05 17:14:21 +0000

    login page created

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,7 +11,43 @@
     <p>Hello, This is login page</p>
 
   </head>
-  <body>
+  <body style="background:#d2d6de;">
+
+    <div class="container">
+    <p><br/></p>
+    <div class="row">
+    <div class="col-md-4"></div>
+    <div class="col-md-4">
+    <div class="panel panel-default">
+    <div class="panel-body">
+    <div class="page-header">
+       <h3>Login Here!</h3>
+    </div>
+    <form>
+    <div class="form-group">
+       <label for="exampleInputEmail1">Email or Username</label>
+    <div class="input-group">
+       <span class="input-group-addon" id="basic-addon1="><span class="glyphicon glyphicon-user"></span></span>
+       <input type="email" class="form-control"  id="exampleInputEmail1" placeholder="Email or Username">
+    </div>
+
+    </div>
+    <div class="form-group">
+       <label for="exampleInputPassword1">Password</label>
+       <div class="input-group">
+       <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-star"></span></span>
+       <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    </div>
+       <hr/>
+       <button type="button" class="btn btn-success"><span class="glyphicon glyphicon-arrow-left"></span>Back</button>
+       <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-lock"></span>Login</button>
+       <p><br/></p>
+    </form>
+                    </div>
+              </div>
+        </div>
+    </div>
 
     <script type="text/javascript" src="items/bootstrap-3.3.5/js/jquery-1.11.3.js"></script>
     <script src="items/bootstrap-3.3.5/js/bootstrap.js"></script>
