e6378eaefc38c8bfdcb8146f5e412b9f588b9cd7
rayeess <p_rayees@hotmail.co.uk>
2015-08-13 21:42:09 +0100

    tidied up the InstructorController

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -19,76 +19,10 @@ class InstructorController extends Controller
      */
     public function getIndex()
     {
-        //
+        //All users who are instructors
         $instructors = User::with('instructor')->has('instructor')->get();
-        // $instructors = User::with('instructor', 'learner')->get();
 
+        //Returning the view with $instructors
         return view('instructor_list', ['instructors' => $instructors]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request  $request
-     * @param  int  $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
