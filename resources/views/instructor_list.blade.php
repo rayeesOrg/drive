45c11b0294fcbc5ffb63bfc80b31a040ff73766c
obaid <obaid@ip-172-31-13-77.us-west-2.compute.internal>
2015-08-22 22:47:02 +0000

    profile added, css changes, ratings changed in instructor_list

--- resources/views/instructor_list.blade.php
+++ resources/views/instructor_list.blade.php
@@ -1,20 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+  <!-- HEAD -->
+  <head>    
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1, user-scalable=no">
+    
+    <!-- GLOBAL STYLES -->
     <link rel="stylesheet" href="{{ URL::asset('items/bootstrap-3.3.5/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('items/bootstrap-3.3.5/css/style.css') }}">
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
-    <title>Drive</title>
+    <!-- END GLOBAL STYLES -->
 
-  </head>
+    <!-- PAGE LEVEL STYLES -->
+    <link rel="stylesheet" href="{{ URL::asset('items/bootstrap-3.3.5/css/style.css') }}">
+    <!-- END PAGE LEVEL STYLES -->
+    <title>Drive</title>
+  </head>     
+  <!-- END HEAD -->
+
+  <!-- BEGIN BODY -->
   <body>
-    <div class="container">
+  <!-- CONTAINER -->
+    <div class="container"> 
       <div class="row">
         <div class="panel panel-default">
+          <!-- PANNEL-HEADING TAG -->
           <div class="panel-heading">
+            <!-- NAVBAR -->
             <nav class="navbar navbar-default" role="navigation">
               <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -26,82 +38,88 @@
                   <a class="navbar-brand" href="#">Company Name</a>
               </div>
               
+              <!-- COLLAPSE NAVBAR-COLLAPSE TAG -->
               <div class="collapse navbar-collapse" id="navbar-collapse-1">
+                <!-- LISTS -->
                 <ul class="nav navbar-nav">
                   <li class="active"><a href="#">Link</a></li>
                   <li><a href="#">Link</a></li>
                   <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                      <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                        <li class="divider"></li>                    
-                      </ul>
-                  </li>
-                </ul>
-        
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                    <!-- DROPDOWN LIST -->
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Action</a></li>
+                      <li><a href="#">Another action</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li class="divider"></li>
+                      <li><a href="#">Separated link</a></li>
+                      <li class="divider"></li>                    
+                    </ul>
+                  </li> <!-- END DROPDOWN LIST -->
+                </ul> <!-- END LISTS -->
+                
+                <!-- col-sm-3 col-md-3 pull-right -->
                 <div class="col-sm-3 col-md-3 pull-right">
-                  <form class="navbar-form" role="search">
+                  <!-- SEARCH BAR FORM -->
+                  <form class="navbar-form" role="search"> 
                     <div class="input-group">
                     <input type="text" class="form-control" placeholder="Search" name="q">
                       <div class="input-group-btn">
                         <button class="btn btn-primary" type="submit"><i class="glyphicon glyphicon-search"></i></button>
                       </div>
                     </div>
-                  </form>
-                </div>        
-              </div>
-            </nav>
-          </div>   
+                  </form> <!-- END SEARCH BAR FORM -->
+                </div> <!-- END col-sm-3 col-md-3 pull-right -->    
+              </div> <!-- END COLLAPSE NAVBAR-COLLAPSE -->
+            </nav> <!-- END NAVBAR -->
+          </div> <!-- END PANNEL-HEADING -->
     
+          <!-- CONATINER1 TAG -->
           <div class="container" id="container1">
             <div class="row feature">
-	        @foreach ($instructors as $instructor)
-              <div class="col-md-3">
-	            <div>
-	              <img src="http://www.gravatar.com/avatar/{{ md5($instructor->email) }}?s=200&d=mm" alt="My avatar" class="img-circle img-thumbnail">
-	              <h4>{{ $instructor->instructor->title }} {{ $instructor->instructor->first_name }} {{ $instructor->instructor->last_name }}</h4>
-	              <p>               
-	              Areas taught: {{ $instructor->instructor->all_locations }}</br>
-	              Mob: {{ $instructor->instructor->mob_no }}</br>
-	              @if($instructor->instructor->tel_no != NULL)
-	              Tel: {{ $instructor->instructor->tel_no }} 
-			      @else
-				  </p>
-			      @endif
-			        <div class="stars">
-					  <input class="star star-5" id="star-5" type="radio" name="star"/>
-					  <label class="star star-5" for="star-5"></label>
-					  <input class="star star-4" id="star-4" type="radio" name="star"/>
-					  <label class="star star-4" for="star-4"></label>
-					  <input class="star star-3" id="star-3" type="radio" name="star"/>
-					  <label class="star star-3" for="star-3"></label>
-					  <input class="star star-2" id="star-2" type="radio" name="star"/>
-					  <label class="star star-2" for="star-2"></label>
-					  <input class="star star-1" id="star-1" type="radio" name="star"/>
-					  <label class="star star-1" for="star-1"></label>
-	                </div>
-	                  <a href="{{ URL::action('InstructorController@getProfile') }}/{{ $instructor->user_id }}" class="btn btn-success lower">View Profile</a>
-	            </div>
-	           </div>	
-	        @endforeach
-            </div>
-          </div>
+	            @foreach ($instructors as $instructor)
+                <div class="col-md-3">
+                  <!-- PROFILE TAG -->
+	                  <div> 
+      	              <img src="http://www.gravatar.com/avatar/{{ md5($instructor->email) }}?s=200&d=mm" alt="My avatar" class="img-circle img-thumbnail">
+      	              <h4>{{ $instructor->instructor->title }} {{ $instructor->instructor->first_name }} {{ $instructor->instructor->last_name }}</h4>
+      	              <p>               
+      	              Areas taught: {{ $instructor->instructor->all_locations }}</br>
+      	              Mob: {{ $instructor->instructor->mob_no }}</br>
+      	              @if($instructor->instructor->tel_no != NULL)
+      	              Tel: {{ $instructor->instructor->tel_no }} 
+			        @else
+				              </p>
+			        @endif
+			              <div class="rating well-sm">
+                      <div class="row">
+                        <div class="rating">
+                          <span class="glyphicon glyphicon-star"></span>
+                          <span class="glyphicon glyphicon-star"></span>
+                          <span class="glyphicon glyphicon-star"></span>
+                          <span class="glyphicon glyphicon-star"></span>
+                          <span class="glyphicon glyphicon-star-empty"></span>
+                        </div>
+                      </div>                  
+                    </div>
+	                    <a href="{{ URL::action('InstructorController@getProfile') }}/{{ $instructor->user_id }}" class="btn btn-success lower">View Profile</a>
+	                  </div> <!-- PROFILE TAG -->
+	              </div>	<!-- END col-md-3 -->
+	            @endforeach
+            </div> <!-- END ROW -->
+          </div> <!-- END CONTAINER -->
 
-		    <div class="panel-footer"> 
+		      <div class="panel-footer"> <!-- FOOTER TAG -->
 	          <div class="form-group">                  
 	          Already Registered ?  <a href="#" >Login here</a>
 	          </div> 
-	        </div>
-        </div> <!-- /.panel panel-default -->
-      </div> <!-- /.row -->
-    </div> <!-- /.container --><!-- /.row -->
+	        </div> <!-- END FOOTER -->
+        </div> <!-- END PANEL PANEL-DEFAULT -->
+      </div> <!-- END ROW -->
+    </div> <!-- END CONTAINER -->
 
     <script type="text/javascript" src="items/bootstrap-3.3.5/js/jquery-1.11.3.js"></script>
     <script src="items/bootstrap-3.3.5/js/bootstrap.js"></script>
 
-  </body>
-</html>
+  </body><!-- END BODY -->
+</html> 
--- resources/views/instructor_profile.blade.php
+++ resources/views/instructor_profile.blade.php
@@ -1,36 +1,90 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
+<html lang="en"> <!-- .html -->
+  <head> <!-- .head -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="{{ URL::asset('items/bootstrap-3.3.5/css/bootstrap.css') }}">
-
+    <link rel="stylesheet" href="{{ URL::asset('items/bootstrap-3.3.5/css/style.css') }}">
     <title>Drive</title>
 
-  </head>
-  <body>
+  </head> <!-- /.head -->
+  
+  <body>   <!--.body -->
+    <div class="container"> <!-- /.Container -->
+      <div class="row"> <!-- /.row -->
+        <div class="panel panel-primary"> <!-- .panel pane-primary -->
+          <div class="panel-heading">
+            <h4 class="panel-title">Instructor Profile</h4>
+          </div>
 
-  	<p>Instructor Profile</p>
+            <div class="panel-body"> <!-- .pannel-body -->
+              <div class="row info">
+                <div class="profile col-md-3">      
+                @if (count($instructor) > 0)
+                  @foreach ($instructor as $profile)                
+                  <img src="http://www.gravatar.com/avatar/{{ md5($profile->email) }}?s=200&d=mm" alt="My avatar" class="img-rounded img-responsive">
+                  <hr>
+                  <button type="submit" class="hire_me btn btn-primary"><span class="glyphicon glyphicon-ok"></span> Hire Me</button></br>
+                  <b class="glyphicon glyphicon-gbp"></b><b>20</b>GBP/hr <i class="glyphicon glyphicon-pencil"></i> 
+                  <hr><h4>Contact info <i class="glyphicon glyphicon-pencil"></i></h4>
+                      <p>
+                        <b class="glyphicon glyphicon-earphone"></b> Mob: 07799654789 </br>
+                        <b class="glyphicon glyphicon-phone-alt"></b> Tel: 02056345678 </br>
+                        <b class="glyphicon glyphicon-envelope"></b> mana_badman@gmail.com </br>
+                      </p>
+                  @endforeach
+                @endif  
+                </div><!-- /.col-md-3 -->
+                
+                <div class="col-md-6">
+                  <blockquote class="info-adj">
+                    <h3 class="name">{{ $profile->instructor->title }} {{ $profile->instructor->first_name }} {{ $profile->instructor->last_name }}</h3> 
+                    <hr><h4>Work Location <i class="glyphicon glyphicon-pencil"></i></h4>
+                      <small><cite title="Source Title">London, united Kingdom<span class="glyphicon glyphicon-map-marker"></span></cite></small>
+                    <h4>Areas Taught <i class="glyphicon glyphicon-pencil"></i></h4>
+                      <small><cite title="Areas">Kingston, Tiwckenham, Whitton, Hounslow, Feltham<span class="glyphicon glyphicon-map-marker"></span></cite></small>
+                    <hr><h4>Additional Info <i class="glyphicon glyphicon-pencil"></i></h4>
+                    <p>
+                      <b class="glyphicon glyphicon-info-sign"></b> Manual & Automatic 
+                    </p>
+                  </blockquote>
+                </div> <!-- /.col-md-6 -->
 
-  	@if (session('status'))
-      <div class="alert alert-success">
-        {{ session('status') }}
-      </div>
-    @endif
+                <div class="col-xs-12 col-md-3 ">
+                  <div class="rating well-sm">
+                    <div class="row">
+                      <h1 class="rating-num">4.0</h1>
+                        <div class="rating">
+                          <span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star">
+                          </span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star">
+                          </span><span class="glyphicon glyphicon-star-empty"></span>
+                        </div>
+                        <div>
+                          <span class="glyphicon glyphicon-user"></span>Reviews: 5
+                        </div>
+                    </div>                  
+                  </div>
+                </div> <!-- /.col-xs-12 col-md-3 -->
+              </div> <!-- /.row -->
+            </div> <!-- /.panel body -->
+            
+            <div class="panel-footer "> <!-- .footer --> 
+              <div class="form-group">
+              Links|Link2|Link3|Link4
+              </div> 
+            </div><!-- /.footer -->
+        </div> <!-- /.panel pane;-primary -->
+      </div> <!-- /.row -->
+    </div> <!-- /.container --><!-- /.row -->
 
-  	@if (count($instructor) > 0)
-	  	@foreach ($instructor as $profile)
-	  		<img src="http://www.gravatar.com/avatar/{{ md5($profile->email) }}?s=200&d=mm" alt="My avatar">
-	  		{{ $profile->instructor->title }} {{ $profile->instructor->first_name }} {{ $profile->instructor->last_name }}
-	  	@endforeach
-	@endif
-
-	<p>
-	<a href="{{ URL::action('InstructorController@getAddVehicle') }}" class="btn btn-default" role="button">Add a vehicle</a>
-	</p>
-	
-  	<script type="text/javascript" src="items/bootstrap-3.3.5/js/jquery-1.11.3.js"></script>
+    <script type="text/javascript" src="items/bootstrap-3.3.5/js/jquery-1.11.3.js"></script>
     <script src="items/bootstrap-3.3.5/js/bootstrap.js"></script>
-   </body>
-</html>
+  </body><!-- /.body -->
+</html><!-- /.html -->
+
+
+
+    
+
+      
