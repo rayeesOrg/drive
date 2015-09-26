dd51ae26f73ccbe5b5ef4afec903dcb6a4fd6ae3
obaid <obaid@ip-172-31-13-77.us-west-2.compute.internal>
2015-09-26 20:02:57 +0000

    home page

--- resources/views/instructor_profile.blade.php
+++ resources/views/instructor_profile.blade.php
@@ -47,7 +47,7 @@
                     <small><cite title="Source Title">{{ $instructor->instructor->work_location }}<span class="glyphicon glyphicon-map-marker"></span></cite></small>
                   <h4>Areas Taught <a href="#" data-target="#editModal" data-toggle="modal"><span class="glyphicon glyphicon-pencil"></span></a></h4>
                     <small><cite title="Areas">{{ $instructor->instructor->all_locations }}<span class="glyphicon glyphicon-map-marker"></span></cite></small>
-                  <hr><h4><i class="glyphicon glyphicon-info-sign"></i>Additional Info <a href="#" data-target="#editModal" data-toggle="modal"><span class="glyphicon glyphicon-pencil"></span></a></h4>
+                  <hr><h4><i class="fa fa-car"></i>Additional Info <a href="#" data-target="#editModal" data-toggle="modal"><span class="glyphicon glyphicon-pencil"></span></a></h4>
                     <p><i>Manual & Automatic</i></p>
                   <hr><h4>Special Offers <a href="#" data-target="#editModal" data-toggle="modal"><span class="glyphicon glyphicon-pencil"></span></a></h4>
                     <p><i>5 Lessons for £75</i></p>
@@ -206,7 +206,40 @@
             </div><!--/col-xs-12 col-md-4--> 
             @endif
           </div>
-   
+
+          
+          <div id="calendar"></div>
+
+          <div id="createEventModal" class="modal hide" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1" aria-hidden="true">
+              <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+                  <h3 id="myModalLabel1">Create Appointment</h3>
+              </div>
+              <div class="modal-body">
+              <form id="createAppointmentForm" class="form-horizontal">
+                  <div class="control-group">
+                      <label class="control-label" for="inputPatient">Patient:</label>
+                      <div class="controls">
+                          <input type="text" name="patientName" id="patientName" tyle="margin: 0 auto;" data-provide="typeahead" data-items="4" data-source="[&quot;Value 1&quot;,&quot;Value 2&quot;,&quot;Value 3&quot;]">
+                            <input type="hidden" id="apptStartTime"/>
+                            <input type="hidden" id="apptEndTime"/>
+                            <input type="hidden" id="apptAllDay" />
+                      </div>
+                  </div>
+                  <div class="control-group">
+                      <label class="control-label" for="when">When:</label>
+                      <div class="controls controls-row" id="when" style="margin-top:5px;">
+                      </div>
+                  </div>
+              </form>
+              </div>
+              <div class="modal-footer">
+                  <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+                  <button type="submit" class="btn btn-primary" id="submitButton">Save</button>
+              </div>
+          </div>
+        
+          
     		  <!--modal img-->
     		  <div tabindex="-1" class="modal fade" id="imgModal" role="dialog" aria-hidden="true">
     		    <div class="modal-dialog">
@@ -414,6 +447,51 @@
     <script>
       $('#send_btn').popover({content: 'Thank You'},'click'); 
     </script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+
+      var calendar = $('#calendar').fullCalendar({
+      defaultView: 'agendaWeek',
+      editable: true,
+        selectable: true,
+      //header and other values
+      select: function(start, end, allDay) {
+          endtime = $.fullCalendar.formatDate(end,'h:mm tt');
+          starttime = $.fullCalendar.formatDate(start,'ddd, MMM d, h:mm tt');
+          var mywhen = starttime + ' - ' + endtime;
+          $('#createEventModal #apptStartTime').val(start);
+          $('#createEventModal #apptEndTime').val(end);
+          $('#createEventModal #apptAllDay').val(allDay);
+          $('#createEventModal #when').text(mywhen);
+          $('#createEventModal').modal('show');
+       }
+    });
+
+  $('#submitButton').on('click', function(e){
+    // We don't want this to act as a link so cancel the link action
+    e.preventDefault();
+
+    doSubmit();
+  });
+
+  function doSubmit(){
+    $("#createEventModal").modal('hide');
+    console.log($('#apptStartTime').val());
+    console.log($('#apptEndTime').val());
+    console.log($('#apptAllDay').val());
+    alert("form submitted");
+        
+    $("#calendar").fullCalendar('renderEvent',
+        {
+            title: $('#patientName').val(),
+            start: new Date($('#apptStartTime').val()),
+            end: new Date($('#apptEndTime').val()),
+            allDay: ($('#apptAllDay').val() == "true"),
+        },
+        true);
+   }
+});
+</script>
   </body>
 </html>
 
